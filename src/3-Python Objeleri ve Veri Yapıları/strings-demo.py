86ae4ff9a45e08b9f123107deda86709b7fda74e
Musatg <musatg9923@gmail.com>
2020-04-26 01:59:51 +0300

    string denemeleri yapıldı

--- src/3-Python Objeleri ve Veri Yapıları/strings-demo.py
+++ src/3-Python Objeleri ve Veri Yapıları/strings-demo.py
@@ -2,39 +2,44 @@ website = "http://www.sadikturan.com"
 course  = "Python Kursu: Baştan Sona Python Programlama Rehberiniz (40 saat)"
 
 # 1- 'course' karakter dizisinde kaç karakter bulunmaktadır ?
-result = len(course)
-length = len(website)
-
+# result = len(course)
+# length = len(website)
+result=len(course)
 # 2- 'website' içinden www karakterlerini alın.
-result = website[7:10]
-
+# result = website[7:10]
+result=website[7:10]
 # 3- 'website' içinden com karakterlerini alın.
-result = website[22:25]
-result = website[length-3:length]
-
+# result = website[22:25]
+# result = website[length-3:length]
+result=website[-3:]
 # 4- 'course' içinden ilk 15 ve son 15 karakterlerini alın.
-result = course[0:15]
-result = course[:15]
-result = course[-15:]
-
+# result = course[0:15]
+# result = course[:15]
+# result = course[-15:]
+result=website[:15]
+result=website[-15:]
 # 5- 'course' ifadesindeki karakterleri tersten yazdırın.
-result = course[::-1]
+# result = course[::-1]
+result=course[::-1]
 
-name, surname, age, job = 'Bora','Yılmaz', 32, 'mühendis' 
-
+# name, surname, age, job = 'Bora','Yılmaz', 32, 'mühendis' 
 # 6- Yukarıda verilen değişkenler ile ekrana aşağıdaki ifadeyi yazdırın.
 #    'Benim adım Bora Yılmaz, Yaşım 32 ve mesleğim mühendis.' 
-
-result = "Benim adım "+ name+ " " + surname+ ", Yaşım "+ str(age) + " ve mesleğim "+ job
-result = "Benim adım {0} {1}, Yaşım {2} ve mesleğim {3}.".format(name,surname,age,job) 
-result = f'Benim adım {name} {surname}, Yaşım {age} ve "mesleğim" {job}.'
+n,sn,a,j='Bora','Yılmaz', 32, 'mühendis' 
+result=f"Benim adım {n} {sn}, Yaşım {a} ve mesleğim {j}."
+# result = "Benim adım "+ name+ " " + surname+ ", Yaşım "+ str(age) + " ve mesleğim "+ job
+# result = "Benim adım {0} {1}, Yaşım {2} ve mesleğim {3}.".format(name,surname,age,job) 
+# result = f'Benim adım {name} {surname}, Yaşım {age} ve "mesleğim" {job}.'
 
 # 7- 'Hello world' ifadesindeki w harfini 'W' ile değiştirin.
-s = 'Hello world'
-s = s[0:6] + 'W'+ s[-4:]
+s='Hello world'
+result=s[:6]+'W'+s[7:]
+# s = 'Hello world'
+# s = s[0:6] + 'W'+ s[-4:]
 
-print(s)
+# print(s)
 # 8- 'abc' ifadesini yan yana 3 defa yazdırın.
-result = 'abc ' * 3
+result='abc'*3
+# result = 'abc ' * 3
 
 print(result)
